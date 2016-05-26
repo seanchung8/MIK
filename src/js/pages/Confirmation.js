7fc7be4a7419c850a72074df988c84ecffb679ad
Damionmccoy <damion.mccoy@skedge.me>
2016-05-26 11:42:17 -0700

    fixed typo in confrimation screen

--- src/js/pages/Confirmation.js
+++ src/js/pages/Confirmation.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-import {AppointmentEdit} from '../components/AppointmentEdit';
-import {MIKHeader} from "../components/MIKHeader";
-import {MIKFooter} from "../components/MIKFooter";
+import AppointmentEdit from '../components/AppointmentEdit';
+import MIKHeader from "../components/MIKHeader";
+import MIKFooter from "../components/MIKFooter";
 
 export default class Confirmation extends React.Component {
 
